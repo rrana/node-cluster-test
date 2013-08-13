7e59863969c9525defa82bda191246d98f2a0f30
rrana <rana@newscred.com>
2013-08-14 05:16:13 +0600

    git ignore incldued. Initial app and server.js included for testing

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-node-cluster-test
+node-clustering-test
 =================
+
+# Clone this project
+
+# Node package installation
+sudo npm install -g cluster
+sudo npm install -g dateformat
+sudo npm install -g forever
+
+# Nginx installation
+sudo apt-get install nginx
+
+# Replace /etc/nginx.conf with nginx-conf/nginx.conf
+# Replace /etc/nginx/sites-enabled/default with nginx-conf/default
+
+# Create log directory
+mkdir -p /var/log/cmc-log
+
+# Execute APP with two worker defined with CLUSTER_WORKERS=<number-of-workers>
+sudo CLUSTER_WORKERS=1 node server.js &
+
+# Run server.js with forever (OPTIONAL)
+sudo CLUSTER_WORKERS=2 forever start -e /var/log/cmc-log/err.log -o /var/log/cmc-log/out.log -a server.js
+
---
+++ server.js
@@ -0,0 +1,42 @@
+// Libraries
+var cluster = require('cluster');
+var datefmt = require('dateformat');
+
+// http://nodejs.org/api/cluster.html
+
+// Setup
+var workers = parseInt(process.env.CLUSTER_WORKERS, 4);
+
+cluster.setupMaster({
+    // exec : "app.js",
+    exec : "app.js",
+});
+
+// Utilities
+function say(message) {
+    console.log("[SERVER] " + message);
+};
+
+// Startup Messaging
+say("Master starting:");
+say("time        => " + datefmt(new Date(), "ddd, dd mmm yyyy hh:MM:ss Z"));
+say("pid         => " + process.pid);
+say("environment => " + process.env.NODE_ENV);
+
+// Fork Workers
+say("Workers starting:");
+
+for (var i = 0; i < workers; i++) {
+    cluster.fork();
+}
+
+// Worker Event Handlers
+cluster.on('exit', function(worker, code, signal) {
+    say('worker      => with pid: ' + worker.process.pid + ', died. Restarting...');
+    cluster.fork();
+});
+
+cluster.on('online', function(worker) {
+    say('worker      => start with pid: ' + worker.process.pid + '.');
+});
+
